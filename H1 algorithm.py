3a89e5f02e6b5f74983e3fc4993ea70c33bf11fb
Razvan-NA <razvannagiu@yahoo.com.com>
2026-03-03 21:49:03 +0200

    finished a star

--- H1 algorithm.py
+++
@@ -1,5 +0,0 @@
-import heapq
-def h1_calculate(table):
-
-def h1_algorithm(table)
-    :
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from Table_Print import print_table
 from shuffle import random_table
-table = random_table()
-print_table(table)
+from A_STAR import a_star, get_path, Node
+start_state = random_table()
+start_node = Node(start_state)
+goal_state = a_star(start_node)
+if goal_state:
+    solution_steps = get_path(goal_state)
+    print(f"Goal reached in {len(solution_steps) - 1} moves!")
+    
+    for i, board in enumerate(solution_steps):
+        print(f"Step {i}:")
+        print_table(board) # Your colored 3x3 print
+else:
+    print("No solution found.")
